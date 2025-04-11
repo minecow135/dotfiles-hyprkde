90f8dcab7d6eca1b974b51b7d8856e99bd93079a
awd <git@awdawd.eu>
2025-04-11 20:37:54 +0200

    changed to download the archinstall script, and add opt to stop restart

--- install/archinstall.sh
+++ install/archinstall.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-# run archinstall script
-./parts/01_archinstall.sh
+while getopts ":n" opt; do
+  case $opt in
+      n) NORESTART=1 ;;
+      ?) echo "ERROR: Invalid option '-$OPTARG' (Valid: n)" >&2; exit 1 ;;
+  esac
+done
 
-# run app install
-./apps.sh
+dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
+mkdir -p $dir
+
+git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+
+# run archinstall script
+$dir/install/parts/01_archinstall.sh
+
+if [[ -z $NORESTART ]]
+then
+  reboot
+fi
