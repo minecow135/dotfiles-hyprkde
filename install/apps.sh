6038826f314580ef7f5b66334c26f3e8c4336aed
awd <git@awdawd.eu>
2025-04-11 19:46:32 +0200

    changed to download the apps script

--- install/apps.sh
+++ install/apps.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
+dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
+mkdir -p $dir
+
+git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+
 # run app install script
-./parts/02_installApps.sh
+$dir/install/parts/02_installApps.sh
 
 # run stow script
-./../stow.sh
+$dir/stow.sh
+
+# run after stow script
+$dir/install/parts/03_afterStow.sh
