9bf83d9094a6a083f2ef6b3c1f8a25f372867397
awd <git@awdawd.eu>
2025-04-12 15:14:22 +0200

    add local mode opt

--- install/archinstall.sh
+++ install/archinstall.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-while getopts ":n" opt; do
+while getopts ":nld:" opt; do
   case $opt in
-      n) NORESTART=1 ;;
-      ?) echo "ERROR: Invalid option '-$OPTARG' (Valid: n)" >&2; exit 1 ;;
+    n) NORESTART=1 ;;
+    l) LOCAL=1 ;;
+    d) DIR=$OPTARG ;;
+    :) echo "ERROR: Option '-$OPTARG' requires an argument" >&2; exit 1 ;;
+    ?) echo "ERROR: Invalid option '-$OPTARG' (Valid: n, l, d)" >&2; exit 1 ;;
   esac
 done
 
-dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
-mkdir -p $dir
+if [[ $DIR ]]
+then
+  dir=$DIR
+else
+  dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
+fi
 
-git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+echo $dir
+
+if [[ -z $LOCAL ]]
+then
+  mkdir -p $dir
+  git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+fi
 
 # run archinstall script
 $dir/install/parts/01_archinstall.sh
