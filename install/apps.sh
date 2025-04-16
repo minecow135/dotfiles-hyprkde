82636e89d37eb81b8c2559799d8eb9327c5cf1cd
awd <git@awdawd.eu>
2025-04-16 02:37:47 +0200

    add optargs

--- install/apps.sh
+++ install/apps.sh
@@ -1,9 +1,29 @@
 #!/bin/bash
 
-dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
-mkdir -p $dir
+while getopts ":nld:" opt; do
+  case $opt in
+    n) NORESTART=1 ;;
+    l) LOCAL=1 ;;
+    d) DIR=$OPTARG ;;
+    :) echo "ERROR: Option '-$OPTARG' requires an argument" >&2; exit 1 ;;
+    ?) echo "ERROR: Invalid option '-$OPTARG' (Valid: n, l, d)" >&2; exit 1 ;;
+  esac
+done
 
-git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+if [[ $DIR ]]
+then
+  dir=$DIR
+else
+  dir=${DOTFILEDIR:-"$HOME/.dotfiles/"}
+fi
+
+echo $dir
+
+if [[ -z $LOCAL ]]
+then
+  mkdir -p $dir
+  git clone https://github.com/minecow135/dotfiles-hyprkde.git $dir
+fi
 
 # run app install script
 $dir/install/parts/02_installApps.sh
@@ -13,3 +33,9 @@ $dir/stow.sh
 
 # run after stow script
 $dir/install/parts/03_afterStow.sh
+
+if [[ -z $NORESTART ]]
+then
+  reboot
+fi
+
