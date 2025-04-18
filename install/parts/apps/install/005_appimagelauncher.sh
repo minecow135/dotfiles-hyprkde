d57123a3969d83244bfc5dfe045b93ea1f248869
awd <git@awdawd.eu>
2025-04-18 13:52:43 +0200

    changed apppimage launcher config to be copied in on install

--- install/parts/apps/install/005_appimagelauncher.sh
+++ install/parts/apps/install/005_appimagelauncher.sh
@@ -1,1 +1,6 @@
+dir=${DOTFILEDIR:-$HOME/.dotfiles}
+
 yay -S --answerdiff None --answerclean None --removemake --noconfirm appimagelauncher
+
+cp $dir/install/parts/lib/appimagelauncher.cfg $HOME/.config/
+
